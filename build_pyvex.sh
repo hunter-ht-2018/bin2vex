0758910eac4655a84739e8cbc56d6b6972be62b2
nemo5566 <ly_nudt@126.com>
2018-05-07 20:08:41 +0800

    fix makefile name

--- build_pyvex.sh
+++ build_pyvex.sh
@@ -2,7 +2,7 @@ export VEX_INCLUDE_PATH=../../vex/pub
 export VEX_LIB_PATH=../../vex
 export MULTIARCH=1
 export DEBUG=1
-myfile="./Makefile"
+myfile="./makefile"
 CWD=`pwd`
 cd vex
 if [ ! -f "$myfile" ]; then
