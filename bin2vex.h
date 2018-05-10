fbaf03cad106908c02b2bbe87acc964dfd14e80c
Xu Zhou <zhouxu.cs@outlook.com>
2018-05-10 20:30:09 +0800

    make it compatible with taint_track as a submodule.

--- bin2vex.h
+++ bin2vex.h
@@ -1,11 +1,19 @@
 #ifndef BIN_2_VEX_H_
 #define BIN_2_VEX_H_
 #include <libvex.h>
-#include <pyvex.h>
+//#include <pyvex.h>
 #include <stdint.h>
 
-void init_bin2vex(VexArch arch);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+void init_bin2vex(VexArch arch);
 IRSB* bin2vex(uint8_t* inst_data, uint64_t inst_addr) ;
+
+#ifdef __cplusplus
+}
+#endif
+
 
 #endif
